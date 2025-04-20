8a2d260638ec9367e621ba21f30195b4b87e78b7
Shodruz Hoshimzoda <shodruzxoshimzoda@gmail.com>
2025-04-21 00:35:42 +0500

    Создал форму для cоздании пользователей и настроил виджеты

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm # формы для авторизации и создании пользователей
 from .models import User
 from django import forms
 
@@ -15,3 +15,26 @@ class UserLoginForm(AuthenticationForm):
         model = User  # будем работать с полем User
         fields = ('username','password')  # Поля в форме
 
+class UserRegistrationForm(UserCreationForm):
+    """Класс формы для создание пользовавтелей"""
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите имя'
+    }))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите фамилию'
+    }))
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'
+    }))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'class': 'form-control py-4', 'placeholder': 'Введите адрес эл. почты'
+    }))
+    password1= forms.CharField(widget=forms.PasswordInput(attrs={
+        'class':'form-control py-4','placeholder':'Введите пароль'
+    }))
+    password2= forms.CharField(widget=forms.PasswordInput(attrs={
+        'class':'form-control py-4','placeholder':'Подьвердите пароль'
+    }))
+    class Meta:
+        model = User
+        fields = ('first_name','last_name','username','email','password1','password2')

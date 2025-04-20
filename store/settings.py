d91cd1c760a7b406dde0703452c4386fe9dda7de
Shodruz Hoshimzoda <shodruzxoshimzoda@gmail.com>
2025-04-20 21:28:56 +0500

    Добавил коннфинурации для работы с пользовательями

--- store/settings.py
+++ store/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'products',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+#
+''' Глобально переопределяем нашего пользователья'''
+
+#   Users
+
+AUTH_USER_MODEL = 'users.User'

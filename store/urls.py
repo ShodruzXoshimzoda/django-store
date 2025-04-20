0ad6c13c1b0eeaaa5b03df0722270d1d4cc48b1f
Shodruz Hoshimzoda <shodruzxoshimzoda@gmail.com>
2025-04-20 21:58:36 +0500

    Добавил основной путь к users

--- store/urls.py
+++ store/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("",views.index,name = "index"),
     path("products/",include('products.urls',namespace='products')),
+    path("users/",include('users.urls',namespace='users')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 

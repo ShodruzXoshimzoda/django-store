696134ddb523c12cde9db5778e91da47ad07cf14
Shodruz Hoshimzoda <shodruzxoshimzoda@gmail.com>
2025-04-20 21:28:27 +0500

    Cоздал модель на основе уже существующего класса

--- users/models.py
+++ users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser  # угследуемся от существующего класса для работы с пользовательями
 
-# Create your models here.
+class User(AbstractUser):
+    ''' Расширяем существующий класс '''
+    image = models.ImageField(upload_to="users_images",null=True, blank=True)
+

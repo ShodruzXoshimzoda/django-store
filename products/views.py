180396b71b44d4b2bdd00ae69f724e4fe00065c3
Shodruz Hoshimzoda <shodruzxoshimzoda@gmail.com>
2025-04-20 18:39:39 +0500

    Из базы данных взял все объекты и передал их в шаблон

--- products/views.py
+++ products/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-
-
+from .models import ProductCategory, Product
 def index(request):
-    context = {'title':'Store'}
+    context = {'title':'Store',}
     return render(request,'products/index.html',context)
 def products(request):
-    context = {'title':'Store - Каталог'}
+    context = {
+        'title':'Store - Каталог',
+        'products':Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
+    }
     return render(request,'products/products.html',context)
 

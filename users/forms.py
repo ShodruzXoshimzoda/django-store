17e972955737f2bc26c8f92d204f2297e30317d1
Shodruz Hoshimzoda <shodruzxoshimzoda@gmail.com>
2025-04-22 11:58:42 +0500

    Добавил форму для профилья пользователья чтобы он сам мог именить свои данные

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm # формы для авторизации и создании пользователей
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm,UserChangeForm # формы для авторизации и создании пользователей и личный кабинеь
 from .models import User
 from django import forms
 
@@ -38,3 +38,15 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('first_name','last_name','username','email','password1','password2')
+
+
+class UserPofileForm(UserChangeForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={ 'class': 'form-control py-4'}))
+    image = forms.ImageField(widget=forms.FileInput(attrs={ 'class': 'custom-file-input'}),required=False) # Поля для изображений не обязательна для заполнения
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4', 'readonly':True}))
+    email = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4',  'readonly':True}))
+
+    class Meta:
+        model = User
+        fields = ('first_name','last_name','image','username','email')

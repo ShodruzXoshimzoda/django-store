21a9c74f24128743c327faf6b4aff057f644f3ab
Shodruz Hoshimzoda <shodruzxoshimzoda@gmail.com>
2025-05-03 19:44:38 +0500

    Настроил Product для админки

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import ProductCategory,Product
+from .models import ProductCategory,Product,Basket
 
-admin.site.register(Product)
+# admin.site.register(Product)
 admin.site.register(ProductCategory)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    '''   Крсивый вывод в админке  '''
+    list_display = ('name','price','quantity','category')
+    fields = ('image','name','description',('price','quantity'),'category')
+    readonly_fields = ('description',)
+    search_fields = ('name',)
+    ordering = ('name',)
+

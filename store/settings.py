9d70714b6382a4f6d40fea742ea4f4c598ada830
Shodruz Hoshimzoda <shodruzxoshimzoda@gmail.com>
2025-05-27 15:29:28 +0500

    Добавил настройки для подключения к базе данных PostgreSQL

--- store/settings.py
+++ store/settings.py
@@ -77,10 +77,23 @@ WSGI_APPLICATION = "store.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+# База данных PostgreSQL
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": 'store_db',
+        'USER': 'store_username',
+        'PASSWORD': 'store_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
